e286ff2b861e71dae2a8b9a88a25abd247c1dca2
Imaad Uddin <imaaduddin27@gmail.com>
2022-08-27 15:30:34 -0500

    setting up the cars movements

--- car.js
+++ car.js
@@ -4,6 +4,14 @@ class Car {
         this.y = y;
         this.width = width;
         this.height = height;
+
+        this.controls = new Controls();
+    }
+
+    update() {
+        if (this.controls.forward) {
+            this.y -= 2;
+        }
     }
 
     draw(ctx) {
