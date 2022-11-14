656f2a21e5b0f736ee80be0c8db6ad49dc30ed2a
Imaad Uddin <imaaduddin27@gmail.com>
2022-11-14 17:40:39 -0600

    resize the car

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
-const car = new Car(100, 100, 20, 50);
+const car = new Car(100, 100, 30, 50);
 car.draw(ctx);
 
 animate();
